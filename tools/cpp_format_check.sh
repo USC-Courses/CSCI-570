69ef532205a60fa5bc0086de8d40f869eb76e79b
XJDKC <xjdkcsq3@gmail.com>
2022-04-12 00:04:48 -0700

    add inplace options for cpp and python format check scripts

--- tools/cpp_format_check.sh
+++ tools/cpp_format_check.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-find src/cpp -name '*.cpp' | xargs clang-format --Werror --dry-run
+INPLACE=0
+while [[ $# -gt 0 ]]; do
+    case "${1}" in
+    -i | --in-place) INPLACE=1 ;;
+    -h | --help)
+        echo "Usage: $(basename "$0") [-i --in-place]"
+        ecit 0
+        ;;
+    *)
+        echo "ERROR: Unrecognized argument: $1"
+        echo "Usage: $(basename "$0") [-i --in-place]"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+if [ ${INPLACE} -eq 0 ]; then
+    find src/cpp -name '*.cpp' | xargs clang-format --Werror --dry-run
+else
+    find src/cpp -name '*.cpp' | xargs clang-format -i
+fi
